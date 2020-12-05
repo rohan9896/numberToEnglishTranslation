dd48d91318c250b89cc8ce6b63c5a5549d808401
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2020-12-05 16:35:22 +0530

    refractor: added script.js

--- script/script.js
+++ script/script.js
@@ -1,8 +1,43 @@
 var input = document.querySelector("#input-number");
 var translateBtn = document.querySelector("#btn");
-var output = document.querySelector("output-number");
+var output = document.querySelector("#output-text");
 
 
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var url = "https://api.funtranslations.com/translate/numbers.json";
 
 
+var urlConstructor = (text) => {
+    return url + "?text=" + text;
+}
+
+
+
+
+var errorHandler = () => {
+    alert("Api limit exceeded, try after some time");
+    console.log("error");
+}
+
+var clickHandler = () => {
+    var inputNum = input.value;
+
+    var reqUrl = urlConstructor(inputNum);
+
+    console.log(reqUrl)
+
+    fetch(reqUrl)
+    .then(response => response.json())
+    .then(data => {
+        var outputText = data.contents.translated;
+        output.innerText = outputText;
+    })
+    .catch(errorHandler);
+
+
+    // console.log(reqUrl)
+
+}
+
+
+
+translateBtn.addEventListener("click", clickHandler)
