310d600a4f5a1bf1498fd91c0f6512587a96a4c1
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2020-12-05 14:51:38 +0530

    style: html and css part completed

---
+++ script/script.js
@@ -0,0 +1,8 @@
+var input = document.querySelector("#input-number");
+var translateBtn = document.querySelector("#btn");
+var output = document.querySelector("output-number");
+
+
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+
